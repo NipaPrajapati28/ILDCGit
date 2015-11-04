97b1e407a3d675227a9de10b025adfa80f3edff4
indianic <indianic@ind697.local>
2015-11-04 12:44:07 -0800

    added appdelegate

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by indianic on 04/11/15.
+//  Copyright © 2015 ildc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by indianic on 04/11/15.
+//  Copyright © 2015 ildc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+@interface ViewController : UIViewController
+
+
+@end
+
